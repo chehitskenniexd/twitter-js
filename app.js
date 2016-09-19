05c0c92e166d7fd3359ffe42fbaa3dd1cf0de502
Andy Vays <andyvays@gmail.com>
2016-09-19 17:42:21 -0400

    cha cha changes

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var Express = require('express');
 var volleyball = require('volleyball');
+var nunjucks = require('nunjucks');
+
 const logger = volleyball.custom({ debug: true })
 
 
 var app = new Express();
 app.use(logger);
+
+nunjucks.configure('views',{noCache: true}); // point nunjucks to the proper directory for templates
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+
 
 app.use(function (req, res, next) {
     console.log('Hello World');
@@ -17,7 +24,18 @@ app.use('/special', function (req, res, next) {
 });
 
 app.get('/', function (req, res, next) {
-    res.send('Hello World!');
+    //res.send('Hello World!');
+    var locals = {
+        title: 'An Example',
+        people: [
+            { name: 'Gandalf'},
+            { name: 'Frodo' },
+            { name: 'Hermione'}
+        ]
+    };
+    nunjucks.render('index.html', locals, function (err, output) {
+        res.render(output);
+    });
 });
 
 app.get('/special', function (req, res, next) {
